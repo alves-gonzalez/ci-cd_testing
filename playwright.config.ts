ab20c459730cffd3c37bc04fab2f39a9c8e6f070
alves-gonzalez <alvesdgonzalez3@gmail.com>
2025-11-09 18:49:09 -0500

    Adding playwright scripts

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,6 +18,13 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
+  /* Configure projects for major browsers */
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    }
+  ],
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
